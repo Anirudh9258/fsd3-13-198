b571837bf810cfff6c91e04b6cdfad0eb3968c60
Anirudh God <anirudhgod@65gmail.com>
2026-07-21 14:47:59 +0530

    add  function

---
+++ LAB 1/hello.js
@@ -0,0 +1,15 @@
+function sayHello(name){
+    console.log(`welcome to ${name}`);
+
+}
+const sayHi = (name) => {
+    console.log(`welcome to ${name}`);
+};
+const findsum = (a,b) => a+b;
+ 
+
+function sum(a,b){
+    return a+b;
+}
+sayHello('Anirudh God');
+console.log(`sum of 2 and 5 is ${sum(2,5)}`);
