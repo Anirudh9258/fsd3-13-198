f7fce3a17e2f02383084adef1039485a2cd66a26
Anirudh God <anirudhgod@65gmail.com>
2026-09-16 12:22:30 +0530

    update in put

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -8,26 +8,31 @@ const server = http.createServer((req,res)=>{
     }
     else if(req.url === '/' && req.method === "POST"){
         //console.log("Request:",req);
-         let body =''
-         req.on("data",(chunk) => {
-            body += chunk;
-         });
-         req.on("end", () => {
-            const product = JSON.parse(body);
-            console.log("received product:",product);
-            res.statusCode = 201;
-            res.end(JSON.stringify({msg:'product added',product}));
-         });
+          
 
          
 
     }
      
 
-    else if(req.url === '/' && req.method === "PUT"){
-        res.statusCode = 200;
-        res.end(" PUT Request");
+    else if(req.url.startsWith("/product/") && req.method === "PUT"){
+       const productID= req.url.split('/').pop();
+       console.log('update product id:',productID);
+       let body =''
+         req.on("data",(chunk) => {
+            body += chunk;
+         });
+         req.on("end", () => {
+            const product = JSON.parse(body);
+            product.id = productID
+            res.statusCode = 200;
+            res.end(JSON.stringify({msg:'product added',product}));
+         });
+
+         
     }
+
+
     else if(req.url === '/' && req.method === "DELETE"){
         res.statusCode = 200;
         res.end( "DELETE Request");
